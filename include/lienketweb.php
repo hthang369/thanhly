dc516ec4224a6b7e8a90486e91fee38263ef97c7
VNNITCOM <nhanit3004@gmail.com>
2021-08-12 19:33:49 +0700

    ok

--- include/lienketweb.php
+++ include/lienketweb.php
@@ -1,7 +1,7 @@
-<div style="color:#F00; font-size:18px">Link Liên Kết Web</div>
-<div style="font-size:11px;">
-<a href="http://suamaytinhvnn.com/" title="SỬA MÁY TÍNH CHUYÊN NGHIỆP" target="_blank"><strong></strong></a>
-<a href="http://www.muinemxehoi.com/" title="BỌC NỆM XE Ô TÔ CHUYÊN NGHIỆP" target="_blank"><strong></strong></a>
-<a href="http://webdesignvnnit.com/" title="THIẾT KẾ WEBSITE CHUYÊN NGHIỆP" target="_blank"><strong></strong></a>
-<a href="http://hathinhphat.com/" title="BÁN HÓA CHẤT ĐỒNG NAI BIÊN HÒA" target="_blank"><strong></strong></a>
+<div style="color:#F00; font-size:15px; padding-top:20PX;">LIÊN KẾT WEBSITE</div>
+<DIV style="color:#F00; font-size:12px;">
+<a href="http://suamaytinhvnn.com/" title="SỬA MÁY TÍNH CHUYÊN NGHIỆP" target="_blank"><strong>SỬA MÁY TÍNH CHUYÊN NGHIỆP</strong></a>
+<a href="http://www.muinemxehoi.com/" title="BỌC NỆM XE Ô TÔ CHUYÊN NGHIỆP" target="_blank"><strong>BỌC NỆM XE Ô TÔ CHUYÊN NGHIỆP</strong></a>
+<a href="http://webdesignvnnit.com/" title="THIẾT KẾ WEBSITE CHUYÊN NGHIỆP" target="_blank"><strong>THIẾT KẾ WEBSITE CHUYÊN NGHIỆP</strong></a>
+<a href="http://hathinhphat.com/" title="BÁN HÓA CHẤT ĐỒNG NAI BIÊN HÒA" target="_blank"><strong>BÁN HÓA CHẤT ĐỒNG NAI BIÊN HÒA</strong></a>
 </div>
