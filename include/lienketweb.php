7d0254fb86b11c6a41ccd43bf759635d6e6a5c1d
VNNITCOM <nhanit3004@gmail.com>
2021-08-12 18:47:15 +0700

    ok

--- include/lienketweb.php
+++ include/lienketweb.php
@@ -1,4 +1,7 @@
 <div style="color:#F00; font-size:18px">Link Liên Kết Web</div>
 <div style="font-size:11px;">
-<a href="http://" title="" target="_blank"><strong></strong></a>
+<a href="http://suamaytinhvnn.com/" title="SỬA MÁY TÍNH CHUYÊN NGHIỆP" target="_blank"><strong></strong></a>
+<a href="http://www.muinemxehoi.com/" title="BỌC NỆM XE Ô TÔ CHUYÊN NGHIỆP" target="_blank"><strong></strong></a>
+<a href="http://webdesignvnnit.com/" title="THIẾT KẾ WEBSITE CHUYÊN NGHIỆP" target="_blank"><strong></strong></a>
+<a href="http://hathinhphat.com/" title="BÁN HÓA CHẤT ĐỒNG NAI BIÊN HÒA" target="_blank"><strong></strong></a>
 </div>
