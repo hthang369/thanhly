b3ffd8082dc2d6fe7504cb15e2f6f8f52c943db0
thang.tran <thang.tran@intrepid.asia>
2024-06-19 09:51:44 +0700

    update 1.2

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,20 +19,25 @@ const mix = require("laravel-mix");
 });
 
 mix
-  // .js('resources/js/app.js', 'public/js')
+  // .js('resources/js/app.js', 'public/js');
   // .js('resources/adminlte_v4/js/adminlte.js', 'public/js/adminlte_v4.js');
-  // .js('resources/js/app_admin.js', 'public/js');
+  // .js('resources/js/app_admin.js', 'public/js').vue();
+  // .ts('resources/ts/main_vue.ts', 'public/js').vue();
   // .js('resources/js/databox.js', 'public/js');
   // .js('resources/js/coreui.js', 'public/js');
   // .js('resources/js/lodash.js', 'public/js');
   // .js('resources/js/test.js', 'public/js');
   // .js('resources/js/main.js', 'public/js');
   // .js('resources/js/nestedSortable.js', 'public/js');
-  .js('resources/js/data-grid.js', 'public/js');
+  // .js('resources/js/data-grid.js', 'public/js');
+  // .js('resources/js/admin.js', 'public/js');
   // .sass('resources/sass/coreui.scss', 'public/css');
   // .sass('resources/adminlte_v4/scss/adminlte.scss', 'public/css/adminlte_v4.css');
   // .sass("resources/sass/app.scss", "public/css");
-  // .sass("resources/sass/home_app.scss", "public/css");
+  // .postCss("resources/sass/tailwind.css", "public/css", [
+  //   require("tailwindcss"),
+  // ]);
+  .sass("resources/sass/home_app.scss", "public/css");
   // .sass("resources/sass/login.scss", "public/css");
   // .sass("resources/sass/admin.scss", "public/css")
   // .css("resources/sass/bootstrap-multiselect.css", 'public/css/bootstrap-multiselect.css')
